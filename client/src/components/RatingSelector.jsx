f3b3b044f040317e70a47721636eade5c3f00886
stdenisj <stdenisjames@gmail.com>
2020-03-12 10:31:47 -0400

    created rating selector component

---
+++ client/src/components/RatingSelector.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class RatingSelector extends Component {
+    selectedOption = (event) => {
+        this.props.inputChange(event);
+    };
+
+    render() {
+        return (
+            <div>
+                <select name="rating" onChange={ this.selectedOption }>
+                    <option value="*">1 star</option>
+                    <option value="**">2 stars</option>
+                    <option value="***">3 stars</option>
+                    <option value="****">4 stars</option>
+                    <option value="*****">5 stars</option>
+                </select>
+            </div>
+        )
+    }
+}
