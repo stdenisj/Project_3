b7bd9671685e3e937ef7d1548dae731db6e9eb69
stdenisj <stdenisjames@gmail.com>
2020-03-11 21:43:03 -0400

    updated restaurant controller to have GET paths for single item and all from database

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -1,10 +1,23 @@
 /* Step 1 import express
  *
  */
-const express = require('express')
-const Restaurant = require('../models/restaurant')
-const restaurantRouter = express.Router()
+const express = require('express');
+const Restaurant = require('../models/restaurant');
+const restaurantRouter = express.Router();
+
+restaurantRouter.get('/', (req, res) => {
+  Restaurant.find().then( (restaurants) => {
+    res.json(restaurants);
+  });
+});
+
+restaurantRouter.get('/:id', (req, res) => {
+  Restaurant.findById(req.params.id).then( (restaurant) => {
+    res.json(restaurant)
+  })
+})
+
 
 module.exports = {
   restaurantRouter
-}
+};
