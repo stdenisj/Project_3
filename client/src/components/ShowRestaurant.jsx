9b89fc26768dd1fea607ade6e0f762b437aa192c
stdenisj <stdenisjames@gmail.com>
2020-03-11 22:49:55 -0400

    added components for list and show single restaurant

---
+++ client/src/components/ShowRestaurant.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ShowRestaurant extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
