523d213db9cc39622874f041be75219b374d3e64
stdenisj <stdenisjames@gmail.com>
2020-03-12 11:46:41 -0400

    created product router and added paths

---
+++ controllers/product.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const Product = require('../models/product');
+const productRouter = express.Router();
+
+productRouter.get('/', (req, res) => {
+    Product.find().then( (products) => {
+        res.json(products);
+    });
+});
+
+productRouter.get('/:restId', (req, res) => {
+    Product.find({ restaurant: req.params.restId }).then( (products) => {
+        res.json(products);
+    });
+});
+
+productRouter.post('/', (req, res) => {
+    Product.create(req.body).then( () => {
+        res.status(200).end();
+    });
+});
+
+productRouter.put('/:productId', (req, res) => {
+    Product.findByIdAndUpdate(req.params.productId, rec.body).then( () => {
+        res.status(200).end();
+    });
+});
+
+
+productRouter.delete('/:productId', (req, res) => {
+    Product.findByIdAndRemove(req.params.productId).then( () => {
+        res.status(200).end();
+    });
+});
+
+module.exports = {
+    productRouter
+};
