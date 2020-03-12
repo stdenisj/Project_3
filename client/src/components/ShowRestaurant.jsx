a0181df1f7b4de762fc5f4b5f19cd02710795cc7
stdenisj <stdenisjames@gmail.com>
2020-03-12 09:48:24 -0400

    created show items

--- client/src/components/ShowRestaurant.jsx
+++ client/src/components/ShowRestaurant.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class ShowRestaurant extends Component {
+    
+    getRestaurant = () => {
+
+        axios.get('res')
+    }
+
+
     render() {
         return (
             <div>
