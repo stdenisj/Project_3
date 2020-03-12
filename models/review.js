c423d2108034f0018bab5dc0edfc67a375fd3324
stdenisj <stdenisjames@gmail.com>
2020-03-12 10:31:20 -0400

    updated model

--- models/review.js
+++ models/review.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Review = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-    },
+    // user: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: 'User',
+    // },
     date: {
         type: Date,
         default: Date.now,
