e8de964bd930b8ffdd442b0e73694c61f523471d
stdenisj <stdenisjames@gmail.com>
2020-03-11 21:57:11 -0400

    updated review schema

--- models/review.js
+++ models/review.js
@@ -2,5 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Review = new Schema({
-    
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    rating: {
+        type: String,
+        default: '***',
+        enum: ['*', '**','***', '****', '*****']
+    },
+    comment: String,
 })
+
+module.exports = mongoose.model('Review', Review)
