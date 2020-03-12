6f3018e4a8d741d939223ccbc36246063c732d78
stdenisj <stdenisjames@gmail.com>
2020-03-11 22:36:40 -0400

    updated controller

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -8,33 +8,42 @@ const restaurantRouter = express.Router();
 restaurantRouter.get('/', (req, res) => {
   Restaurant.find().then( (restaurants) => {
     res.json(restaurants);
+  }).catch( (e) => {
+    console.log(e)
   });
 });
 
 restaurantRouter.get('/:id', (req, res) => {
   Restaurant.findById(req.params.id).then( (restaurant) => {
     res.json(restaurant);
+  }).catch( (e) => {
+    console.log(e)
   });
 });
 
 restaurantRouter.post('/', (req, res) => {
   Restaurant.create(req.body).then( () => {
     res.status(200).end();
+  }).catch( (e) => {
+    console.log(e)
   });
 });
 
 restaurantRouter.put('/:id', (req, res) => {
   Restaurant.findByIdAndUpdate(req.params.id, req.body).then( () => {
     res.status(200).end();
+  }).catch( (e) => {
+    console.log(e)
   });
 });
 
 restaurantRouter.delete('/:id'), (req, res) => {
   Restaurant.findByIdAndRemove(req.params.id).then( () => {
     res.status(200).end();
+  }).catch( (e) => {
+    console.log(e)
   });
 };
-g
 
 module.exports = {
   restaurantRouter
